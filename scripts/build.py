8c9b5d86d1170583f231ee186c96ae5e42cd5332
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-12-31 16:56:09 +0100

    refactor: Linting

--- scripts/build.py
+++ scripts/build.py
@@ -1,21 +1,14 @@
 import json
-from pathlib import Path, PosixPath
+import os
 import re
 import shutil
+from pathlib import Path, PosixPath
+
+import minify_html
 from jinja2 import Environment, FileSystemLoader, Template, pass_context
 from markupsafe import Markup
-import minify_html
 
-
-OUTPUT_FOLDER = Path(__file__).parent.parent / "output"
-STATIC_FOLDER = OUTPUT_FOLDER / "static"
-try:
-    shutil.rmtree(OUTPUT_FOLDER)
-except FileNotFoundError:
-    pass
-OUTPUT_FOLDER.mkdir(parents=True)
-
-shutil.copytree(Path(__file__).parent.parent / "static", STATIC_FOLDER)
+OUTPUT_FOLDER = Path(__file__).parent.parent / ("public" if "GITLAB_CI" in os.environ else "output")
 
 
 def format_day_html(value):
@@ -38,12 +31,15 @@ folders = [
     "personal/2024",
 ]
 
-env = Environment(loader=FileSystemLoader('templates'))
-env.filters.update({
-    "format_day": format_day,
-    "format_day_html": format_day_html,
-    "static": static,
-})
+env = Environment(loader=FileSystemLoader("templates"))
+env.filters.update(
+    {
+        "format_day": format_day,
+        "format_day_html": format_day_html,
+        "static": static,
+    }
+)
+
 
 def render(template: Template, output_url: str, params=None, data_folder: Path | None = None):
     output_file = OUTPUT_FOLDER / output_url
@@ -65,33 +61,48 @@ def render(template: Template, output_url: str, params=None, data_folder: Path |
 
     output_file.write_text(output, encoding="utf-8")
 
-for folder in folders:
-    template_folder = "personal/2023" if folder == "personal/2024" else folder
-    days = range(1, 25 + 1)
-    render(env.get_template(f"{template_folder}/home.html"), f"{folder}/index.html", {"days": days})
 
-    template = env.get_template(f"{template_folder}/day.html")
-    for day in days:
-        render(template, f"{folder}/{day}/index.html", {"day": day}, Path(folder))
-
-for file in STATIC_FOLDER.glob("**/*"):
-    prefix = {
-        ".html": "",
-        ".css": "<style>",
-        ".js": "<script>",
-    }.get(file.suffix)
-    if prefix is None:
-        continue
-    data = file.read_text("utf-8")
-    print("Minifying", file)
+def main():
+    STATIC_FOLDER = OUTPUT_FOLDER / "static"
     try:
-        data = minify_html.minify(
-            prefix + data,
-            do_not_minify_doctype=True,
-            minify_css=True,
-            minify_js=True,
-        ).removeprefix(prefix)
-    except:
+        shutil.rmtree(OUTPUT_FOLDER)
+    except FileNotFoundError:
         pass
-    else:
-        file.write_text(data, "utf-8")
+    OUTPUT_FOLDER.mkdir(parents=True)
+
+    shutil.copytree(Path(__file__).parent.parent / "static", STATIC_FOLDER)
+
+    for folder in folders:
+        template_folder = "personal/2023" if folder == "personal/2024" else folder
+        days = range(1, 25 + 1)
+        render(env.get_template(f"{template_folder}/home.html"), f"{folder}/index.html", {"days": days})
+
+        template = env.get_template(f"{template_folder}/day.html")
+        for day in days:
+            render(template, f"{folder}/{day}/index.html", {"day": day}, Path(folder))
+
+    for file in STATIC_FOLDER.glob("**/*"):
+        prefix = {
+            ".html": "",
+            ".css": "<style>",
+            ".js": "<script>",
+        }.get(file.suffix)
+        if prefix is None:
+            continue
+        data = file.read_text("utf-8")
+        print("Minifying", file)
+        try:
+            data = minify_html.minify(
+                prefix + data,
+                do_not_minify_doctype=True,
+                minify_css=True,
+                minify_js=True,
+            ).removeprefix(prefix)
+        except:  # noqa
+            pass
+        else:
+            file.write_text(data, "utf-8")
+
+
+if __name__ == "__main__":
+    main()
